7ddefc3c439d5eddcb49be52f3c7b9b4fca081f2
Todd Motto <todd@toddmotto.com>
2014-07-09 10:36:28 +0100

    Update react.fluidvids.js

--- react.fluidvids.js
+++ react.fluidvids.js
@@ -2,24 +2,24 @@
  * @jsx React.DOM
  */
 var FluidVid = React.createClass({
+  getDefaultProps: function () {
+    return {
+      src : '',
+      width: 1600,
+      height: 900
+    }
+  },
   getRatio: function (width, height) {
     return ((parseInt(height, 10) / parseInt(width, 10)) * 100) + '%';
   },
   render: function() {
-
-    var src = this.props.src || '';
-    var width = this.props.width || 1600;
-    var height = this.props.height || 900;
-
     var ratio = {
       paddingTop: this.getRatio(width, height)
     };
-
     return (
       <div className={ 'fluidvids' } style={ ratio }>
-        <iframe src={ src } width={ width } height={ height } frameBorder={ 0 }></iframe>
+        <iframe src={ this.props.src } width={ this.props.width } height={ this.props.height } frameBorder={ 0 }></iframe>
       </div>
     );
-
   }
 });
